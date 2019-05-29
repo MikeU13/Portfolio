f35aa8b005be95d60fe6d6a14ba3191ed43cc1c5
MikeU13 <51176410+MikeU13@users.noreply.github.com>
2019-05-29 16:42:09 -0400

    Add files via upload
    This script is modified from a Youtube tutorial to work in my test game (link in script comments). This allows the player to collide with the key object, destroy it, and set the player's haveKey variable to true.

---
+++ keyPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Modified from Chris' Tutorials on Youtube https://www.youtube.com/watch?v=12AbcoLrYKM
+public class keyPickup : MonoBehaviour
+{
+    //public GameItem item;     needs to be set up!
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerMovement player = collision.GetComponent<PlayerMovement>();
+
+        if(player != null)
+        {
+            player.haveKey = true;
+            //  player.inventory.add(item);
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
