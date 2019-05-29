f4ae3e25758b51725ffc431f5ea118cfebb237a2
MikeU13 <51176410+MikeU13@users.noreply.github.com>
2019-05-29 16:37:28 -0400

    Add files via upload
    This is a piece of C# code from a game project I am working on with a friend. We needed a box collider trigger to check if the player had the correct key (hasKey == true), then destroy the trigger and open the two doors, allowing the player to leave the room. All code came from Unity documentation, just trial and error until we could get it to work.

---
+++ doorOpen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class doorOpen : MonoBehaviour
+{
+
+
+    public GameObject doorLeft;
+    public GameObject doorRight;
+       
+    
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerMovement player = collision.GetComponent<PlayerMovement>();
+
+        if (player.haveKey == true)
+        {
+            Destroy(gameObject);
+            doorLeft.transform.position += new Vector3(-.74f, 0, 0);
+            doorRight.transform.position += new Vector3(.74f, 0, 0);
+            //door.transform.position = Vector3(door.transform.position, Time.deltaTime * speed);
+
+        }
+        else
+        {
+            Debug.Log("The door will not open");
+        }
+            
+        
+    }
+
+   
+}
+
